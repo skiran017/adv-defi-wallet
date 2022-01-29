143d0bede62a205d39548a3702a959db9c87d819
skiran017 <skiran017@gmail.com>
2022-01-29 18:15:25 +0530

    Smart contracts
    -Compound integration,
    -Deposit ERC20 tokens,
    -Deposit ETH,
    -Withdraw ERC20 token,
    -withdraw ETH

---
+++ contracts/CEthInterface.sol
@@ -0,0 +1,13 @@
+//"SPDX-License-Identifier:UNLICENSED"
+pragma solidity ^0.7.3;
+
+interface CEthInterface {
+    function mint() external payable;
+
+    function redeemUnderlying(uint256 redeemAmount)
+        external
+        view
+        returns (uint256);
+
+    function balanceOfUnderlying(address owner) external returns (uint256);
+}
---
+++ contracts/Compound.sol
@@ -0,0 +1,85 @@
+//"SPDX-License-Identifier:UNLICENSED"
+pragma solidity ^0.7.3;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./CTokenInterface.sol";
+import "./CEthInterface.sol";
+import "./ComptrollerInterface.sol";
+
+contract Compound {
+    ComptrollerInterface public comptroller;
+    CEthInterface public cEth;
+
+    constructor(address _comptroller, address _cEthAddress) {
+        comptroller = ComptrollerInterface(_comptroller);
+        cEth = CEthInterface(_cEthAddress);
+    }
+
+    function supply(address cTokenAddress, uint256 underlyingAmount) internal {
+        CTokenInterface cToken = CTokenInterface(cTokenAddress);
+        address underlyingAddress = cToken.underlying();
+        IERC20(underlyingAddress).approve(cTokenAddress, underlyingAmount);
+        uint256 result = cToken.mint(underlyingAmount);
+        require(
+            result == 0,
+            "cToken#mint() failed. see Compound ErrorReporter.sol for details"
+        );
+    }
+
+    function supplyEth(uint256 underlyingAmount) internal {
+        cEth.mint{value: underlyingAmount}();
+    }
+
+    function redeem(address cTokenAddress, uint256 underlyingAmount) internal {
+        CTokenInterface cToken = CTokenInterface(cTokenAddress);
+        uint256 result = cToken.redeemUnderlying(underlyingAmount);
+        require(
+            result == 0,
+            "cToken#redeemUnderlying() failed. see Compound ErrorReporter.sol for more details"
+        );
+    }
+
+    function redeemEth(uint256 underlyingAmount) internal {
+        uint256 result = cEth.redeemUnderlying(underlyingAmount);
+        require(
+            result == 0,
+            "cEth#redeemUnderlying() failed. see Compound ErrorReporter.sol for more details"
+        );
+    }
+
+    function claimComp() internal {
+        comptroller.claimComp(address(this));
+    }
+
+    function getCompAddress() internal view returns (address) {
+        return comptroller.getCompAddress();
+    }
+
+    function getUnderlyingAddress(address cTokenAddress)
+        internal
+        view
+        returns (address)
+    {
+        return CTokenInterface(cTokenAddress).underlying();
+    }
+
+    function getcTokenBalance(address cTokenAddress)
+        public
+        view
+        returns (uint256)
+    {
+        return CTokenInterface(cTokenAddress).balanceOf(address(this));
+    }
+
+    function getUnderlyingBalance(address cTokenAddress)
+        public
+        returns (uint256)
+    {
+        return
+            CTokenInterface(cTokenAddress).balanceOfUnderlying(address(this));
+    }
+
+    function getUnderlyingEthBalance() public returns (uint256) {
+        return cEth.balanceOfUnderlying(address(this));
+    }
+}
